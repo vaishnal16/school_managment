1d862f8bbca711f26eeba10e4147cd12008b62b7
Vaishnal Mali <vaishnal16305@gmail.com>
2025-05-25 13:24:00 +0530

    changes in server and db

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use('/', schoolRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${MYSQLPORT}`);
 });
